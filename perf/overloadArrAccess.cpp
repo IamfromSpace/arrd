ef77d9936c03dd136e145c04868b69aac16efab3
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2016-03-10 22:06:26 -0800

    Add new perf test for any size arrays with changeable start points.

--- perf/overloadArrAccess.cpp
+++ perf/overloadArrAccess.cpp
@@ -4,6 +4,17 @@ const uint16_t RUNS = 100;
 const uint32_t ITERS = 10000;
 
 template<typename T>
+class arrd31_withStart {
+  public:
+    T arr[31];
+    uint8_t len;
+    uint8_t start;
+    T& operator[] (const int i) {
+      return arr[(i + start) % 31];
+    }
+};
+
+template<typename T>
 class arrd32_noSafety {
   public:
     T arr[32];
@@ -48,6 +59,7 @@ template<template<typename> class T, typename A> void map_cheap(A (*f)(A), T<A> 
 arrd32_noSafety<int> a = {{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113}, 30};
 arrd32_withSafety<int> b = {{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113}, 30};
 arrd32_withStart<int> c = {{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131}, 30, 7};
+arrd31_withStart<int> d = {{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127}, 30, 7};
 
 BENCHMARK(overloadArrAccess, noSafety, RUNS, ITERS) {
   map_cheap(asIs, a);
@@ -59,4 +71,8 @@ BENCHMARK(overloadArrAccess, withSafety, RUNS, ITERS) {
 
 BENCHMARK(overloadArrAccess, withStart, RUNS, ITERS) {
   map_cheap(asIs, c);
+}
+
+BENCHMARK(overloadArrAccess, anySizeWithStart, RUNS, ITERS) {
+  map_cheap(asIs, d);
 }
